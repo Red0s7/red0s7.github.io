87cb1ac43c0d60adf7a64dd6779f368a58b2fdb9
Red0s7 <hecklol4566@gmail.com>
2025-08-27 16:07:45 -0700

    1

--- main1.js
+++ main1.js
@@ -1,10 +1,3 @@
-const HEADER_HEIGHT = ((document.querySelector('header').getBoundingClientRect().height / window.innerHeight) * 100).toFixed(2);
-const HR_HEIGHT = ((document.querySelector('hr').getBoundingClientRect().height / window.innerHeight) * 100).toFixed(2);
-const MAIN_HEIGHT = (100 - HEADER_HEIGHT - HR_HEIGHT).toFixed(2);
-console.log(MAIN_HEIGHT)
-console.log(HEADER_HEIGHT)
-console.log(HR_HEIGHT)
-document.querySelector('main').style.height = `${MAIN_HEIGHT}vh`;
 
 function updateLinks(addSuffix) {
     const links = document.querySelectorAll('a[href]');
