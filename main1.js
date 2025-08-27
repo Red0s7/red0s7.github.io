d4f711b0d46643e902a6f00c1bf7335fa88b83ca
Red0s7 <hecklol4566@gmail.com>
2025-08-26 21:40:26 -0700

    added securly bypass

--- main1.js
+++ main1.js
@@ -5,3 +5,34 @@ console.log(MAIN_HEIGHT)
 console.log(HEADER_HEIGHT)
 console.log(HR_HEIGHT)
 document.querySelector('main').style.height = `${MAIN_HEIGHT}vh`;
+
+function updateLinks(addSuffix) {
+    const links = document.querySelectorAll('a[href]');
+    links.forEach(link => {
+        let href = link.getAttribute('href');
+        if (addSuffix) {
+            if (!href.endsWith('?scrlcbp=.')) {
+                link.setAttribute('href', href + '?scrlcbp=.');
+            }
+        } else {
+            // Remove the suffix if present
+            link.setAttribute('href', href.replace('?scrlcbp=.', ''));
+        }
+    });
+}
+
+const scbypss = document.querySelector("#scbypss");
+
+// Restore toggle state from localStorage
+if (localStorage.getItem('scbypssChecked') === 'true') {
+    scbypss.checked = true;
+    updateLinks(true);
+} else {
+    updateLinks(false);
+}
+
+// Listen for changes
+scbypss.addEventListener('change', function() {
+    localStorage.setItem('scbypssChecked', scbypss.checked);
+    updateLinks(scbypss.checked);
+});
